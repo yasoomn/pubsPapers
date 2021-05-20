6bb4b51c1472ab784b06d2d8ea54f9b83f867163
Yasoo <yasoo4253@gmail.com>
2021-05-20 08:09:02 +0200

    Added feature to add new papers to the database from the DOI

--- papers.sh
+++ papers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPTIONS="copy citation;copy key;copy DOI"
+OPTIONS="copy citation;copy key;copy DOI;add new from DOI"
 MENU=$(echo ${OPTIONS[@]} | sed 's/;/\n/g' | wofi --show=dmenu --insensitive)
 
 if [ "$MENU" == "copy citation" ]
@@ -8,6 +8,7 @@ then
 
     PUB=$(pubs list | wofi --show=dmenu --insensitive)
     CITATION=$(echo $PUB | cut -d "]" -f 2)
+
     echo $CITATION | wl-copy
     exit
 elif [ "$MENU" == "copy key" ]
@@ -17,6 +18,17 @@ then
     echo $KEY
     echo $KEY | wl-copy
     exit
+elif [ "$MENU" == "add new from DOI" ]
+then
+    DOI=$(wofi --show dmenu)
+    ADD=$(pubs add -D "$DOI" )
+    echo $ADD
+    NOTIFY=$(echo $ADD | grep -oP '".*"' )
+    echo $NOTIFY
+    notify-send "Paper added to library"
+    exit
 fi
+
+
 
 
